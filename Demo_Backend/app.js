e15f24ee7fd7fadc01283a3fda6624d1c0a37c3c
Chamara Dilshan <chamaradilshan0011@gmail.com>
2024-02-19 20:17:27 +0530

    add-demo-backend

---
+++ Demo_Backend/app.js
@@ -0,0 +1,46 @@
+const express =  require('express');
+const app = express();
+const cors = require('cors');
+const controller = require('./controller')
+
+app.use(cors());
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+
+app.use(express.json());
+
+//Create first rest API
+app.get('/users', (req, res) =>{
+    controller.getUsers(req, res, next => {
+        res.send();
+    });
+});
+
+//Create second rest API
+app.post('/createuser', (req, res) => {
+    controller.addUser(req.body, (callback) =>{
+        res.send();
+    });
+        
+});
+
+app.post('/updateuser', (req, res) => {
+    controller.updateUser(req.body, (callback) =>{
+        res.send(callback);
+    });
+        
+});
+
+app.post('/deleteeuser', (req, res) => {
+    controller.deleteUser(req.body, (callback) =>{
+        res.send(callback);
+    });
+        
+});
+
+module.exports = app; 
+

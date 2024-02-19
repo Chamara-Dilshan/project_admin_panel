66f6266e01a9ac28354fb5e339176c9ea5267556
Chiran Sooryakumara <85780849+Fernandophcsg@users.noreply.github.com>
2024-02-19 15:01:54 +0530

    update login and signup components

--- src/Pages/Login&SignUp/LoginSignUp.js
+++ src/Pages/Login&SignUp/LoginSignUp.js
@@ -1,5 +1,7 @@
 import React,{useState} from 'react'
 import './LoginSignUp.css'
+import Login from '../../Components/Login'
+import Signup from '../../Components/Signup'
 
 function LoginSignUp() {
     const [active, setActive] = useState(false)
@@ -19,38 +21,8 @@ function LoginSignUp() {
     <>
     <div className="l-s-container" >
         <div className={active ? "ls-container active" : "ls-container"} >
-            <div className="form-container sign-up">
-                <form action='POST'>
-                <h1>Create Account</h1>
-                    <div className="social-icons">
-                        <a href="#" className="icon"><i className="fa-brands fa-google-plus-g"></i></a>
-                        <a href="#" className="icon"><i className="fa-brands fa-facebook-f"></i></a>
-                        <a href="#" className="icon"><i className="fa-brands fa-github"></i></a>
-                        <a href="#" className="icon"><i className="fa-brands fa-linkedin-in"></i></a>
-                    </div>
-                    <span>or use your email for registeration</span>
-                    <input type="text" placeholder="Name"/>
-                    <input type="email" placeholder="Email"/>
-                    <input type="password" placeholder="Password"/>
-                    <button onClick={clickSignIn}>Sign Up</button>
-                </form>
-            </div>
-            <div className="form-container sign-in">
-                <form action='POST'>
-                <h1>Sign In</h1>
-                    <div className="social-icons">
-                        <a href="#" className="icon"><i className="fa-brands fa-google-plus-g"></i></a>
-                        <a href="#" className="icon"><i className="fa-brands fa-facebook-f"></i></a>
-                        <a href="#" className="icon"><i className="fa-brands fa-github"></i></a>
-                        <a href="#" className="icon"><i className="fa-brands fa-linkedin-in"></i></a>
-                    </div>
-                    <span>or use your email password</span>
-                    <input type="email" placeholder="Email"/>
-                    <input type="password" placeholder="Password"/>
-                    <a href="#">→ Forget Your Password? ←</a>
-                    <button onClick={clickSignIn}>Sign In</button>
-                </form>
-            </div>
+            <Signup clickSignIn={clickSignIn}/>
+            <Login clickSignIn={clickSignIn}/>
             <div className="toggle-container">
                 <div className="toggle">
                     <div className="toggle-panel toggle-left">

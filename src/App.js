7ef56f54683a15595e450814c768f7eb00aa7456
Chiran Sooryakumara <85780849+Fernandophcsg@users.noreply.github.com>
2024-02-19 14:38:13 +0530

    add login and signup page

--- src/App.js
+++ src/App.js
@@ -6,14 +6,18 @@ import Users from './Pages/Users/Users';
 import Teams from './Pages/Teams/Teams';
 import Projects from './Pages/Projects/Projects';
 import Clients from './Pages/Clients/Clients';
+import LoginSignUp from './Pages/Login&SignUp/LoginSignUp';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Routes>
+        <Route path='/' element={<LoginSignUp/>}/>
+        </Routes>
         <Sidebar>
           <Routes>
-            <Route path='/' element={<Home/>}/>
+            <Route path='/home' element={<Home/>}/>
             <Route path='/users' element={<Users/>}/>
             <Route path='/teams' element={<Teams/>}/>
             <Route path='/projects' element={<Projects/>}/>

099e84af904662bffa94ec78719980c8fa88344a
Chiran Sooryakumara <85780849+Fernandophcsg@users.noreply.github.com>
2024-02-23 02:08:10 +0530

    update logo

--- Frontend/src/Components/Sidebar.js
+++ Frontend/src/Components/Sidebar.js
@@ -7,6 +7,7 @@ import { FaUser } from "react-icons/fa";
 import { MdGroups2 } from "react-icons/md";
 import { GoProject } from "react-icons/go";
 import { FaUserGear } from "react-icons/fa6";
+import logo from '../images/pngwing.png';
     
     const Sidebar = ({children}) => {
         const[isOpen ,setIsOpen] = useState(false);
@@ -44,10 +45,10 @@ import { FaUserGear } from "react-icons/fa6";
         ]
         return (
             <div className="container">
-               <div style={{width: isOpen ? "250px" : "50px"}} className="sidebar">
+               <div style={{width: isOpen ? "250px" : "55px",overflow:'hidden'}} className="sidebar">
                    <div className="top_section">
-                       <h1 style={{display: isOpen ? "block" : "none"}} className="logo">Logo</h1>
-                       <div style={{marginLeft: isOpen ? "70px" : "0px"}} className="bars">
+                       <img src={logo} width={120} style={{display: isOpen ? "block" : "none"}} className="logo" alt='logo'/>
+                       <div style={{marginLeft: isOpen ? "30px" : "13px",transition:"all 0.5s ease"}} className="bars">
                            <FaBars onClick={toggle}/>
                        </div>
                    </div>
@@ -55,7 +56,7 @@ import { FaUserGear } from "react-icons/fa6";
                        menuItem.map((item, index)=>(
                            <NavLink to={item.path} key={index} className="link" activeclassName="active">
                                <div className="icon" style={{marginRight:'20px'}}>{item.icon}</div>
-                               <div style={{display: isOpen ? "block" : "none",}} className="link_text">{item.name}</div>
+                               <div className="link_text">{item.name}</div>
                            </NavLink>
                        ))
                    }
